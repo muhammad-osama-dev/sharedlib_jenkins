88ad8eceee1438d102fea7a4992e6cc37f28fd20
Muhammad Osama <thegame9555@gmail.com>
2023-09-22 18:11:06 +0300

    Update installnginx.groovy

--- vars/installnginx.groovy
+++ vars/installnginx.groovy
@@ -1,22 +1,24 @@
-pipeline {
-    agent { label 'agent1' }
-    stages {
-        stage('install_nginx') { 
-            steps {
-                echo 'btngan'
-                sh 'sudo apt-get install nginx -y'
+def call() {
+    pipeline {
+        agent { label 'agent1' }
+        stages {
+            stage('install_nginx') { 
+                steps {
+                    echo 'btngan'
+                    sh 'sudo apt-get install nginx -y'
+                }
             }
-        }
-        stage('enable_firewall') { 
-            steps {
-                sh "sudo ufw allow 'Nginx HTTP' -y"
-                sh 'sudo rm /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default /var/www/html/index.nginx-debian.html'
+            stage('enable_firewall') { 
+                steps {
+                    sh "sudo ufw allow 'Nginx HTTP' -y"
+                    sh 'sudo rm /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default /var/www/html/index.nginx-debian.html'
+                }
             }
-        }
-        stage('Test') { 
-            steps {
-                echo 'btngan2'
-                sh 'sudo systemctl nginx status' 
+            stage('Test') { 
+                steps {
+                    echo 'btngan2'
+                    sh 'sudo systemctl nginx status' 
+                }
             }
         }
     }
